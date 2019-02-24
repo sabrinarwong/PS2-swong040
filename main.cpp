0a952df367b80897e97df365abdb5f02caba5ea7
Sabrina Wong <swong040@ucr.edu>
2019-02-24 09:48:03 -0800

    extended index include stemming, parsed terms from ap89, created index.txt listing terms in index

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "main.h"
+
+using namespace std;
+
+#include <ctype.h>
+
+// int main(){
+
+// 	// create index here
+// 	// cout << "Creating index." << endl;
+// 	docIndex *index = new docIndex();
+
+// 	// index->print_index(); // testing purposes
+// 	cout << "Index created." << endl << endl;
+
+
+// 	cout << "Enter a term to query: " << endl;
+
+// 	string input = "";
+// 	cin >> input; transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+// 	while (input != "quit"){
+// 		// test user input here
+// 		index->test(input);
+
+// 		cout << endl;
+// 		cout << "Enter a term to query: " << endl;
+// 		cin >> input; transform(input.begin(), input.end(), input.begin(), ::tolower);
+// 	}
+
+// 	return 0;
+// }
